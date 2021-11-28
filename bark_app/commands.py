b0532c843fa4642b633b120b292a65fac196a4af
Joseph Sergent <wingwhiper@gmail.com>
2021-11-27 20:28:03 -0600

    Initial commit
    Initial commit of bark app files.

---
+++ bark_app/commands.py
@@ -0,0 +1,90 @@
+import sys
+
+import requests
+
+from database import DatabaseManager
+from datetime import datetime
+from abc import ABC, abstractmethod
+
+db = DatabaseManager('bookmarks.db')
+
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self, data):
+        raise NotImplementedError('Commands must implement an execute method')
+
+
+class CreateBookmarksTableCommand(Command):
+    def execute(self, data=None):
+        db.create_table('bookmarks', {
+            'id': 'integer primary key autoincrement',
+            'title': 'text not null',
+            'url': 'text not null',
+            'notes': 'text',
+            'date_added': 'text not null',
+        })
+
+
+class AddBookmarkCommand(Command):
+    def execute(self, data, timestamp=None):
+        data['date_added'] = timestamp or datetime.utcnow().isoformat()
+        db.add('bookmarks', data)
+        return 'Bookmark added!'
+
+
+class ListBookmarksCommand(Command):
+    def __init__(self, order_by='date_added'):
+        self.order_by = order_by
+
+    def execute(self, data=None):
+        return db.select('bookmarks', order_by=self.order_by).fetchall()
+
+
+class DeleteBookmarkCommand(Command):
+    def execute(self, data):
+        db.delete('bookmarks', {'id': data})
+        return 'Bookmark delete!'
+
+
+class ImportGitHubStarsCommand(Command):
+    def _extract_bookmark_info(self, repo):
+        return {
+            'title': repo['name'],
+            'url': repo['html_url'],
+            'notes': repo['description'],
+        }
+
+    def execute(self, data):
+        bookmarks_imported = 0
+
+        github_username = data['github_username']
+        next_page_of_results = f'https://api.github.com/users/{github_username}/starred'
+
+        while next_page_of_results:
+            stars_response = requests.get(next_page_of_results,
+                                          headers={'Accept': 'application/vnd.github.v3.star+json'},)
+            next_page_of_results = stars_response.links.get('next', {}).get('url')
+            for repo_info in stars_response.json():
+                repo = repo_info['repo']
+
+                if data['preserve_timestamps']:
+                    timestamp = datetime.strptime(
+                        repo_info['starred_at'],
+                        '%Y-%m-%dT%H:%M:%SZ'
+                    )
+                else:
+                    timestamp = None
+
+                bookmarks_imported += 1
+                AddBookmarkCommand().execute(
+                    self._extract_bookmark_info(repo),
+                    timestamp=timestamp,
+                )
+
+        return f'Imported {bookmarks_imported} bookmarks from starred repos!'
+
+
+class QuitCommand(Command):
+    def execute(self, data=None):
+        sys.exit()
