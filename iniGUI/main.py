4003ae722bd8c9b721ecc4202c29a037f45ba365
Joseph Sergent <77675582+JJSerg@users.noreply.github.com>
2023-02-19 11:37:17 -0600

    Added function to get user choice on INI File

--- iniGUI/main.py
+++ iniGUI/main.py
@@ -1,6 +1,19 @@
 import tkinter as tk
+from tkinter import filedialog
 
+
+# Functions
+def select_file():
+    filename = filedialog.askopenfilename(title="Select INI File", filetypes=[("INI files", "*.ini")])
+    print("Selected file:", filename)
+
+
+# Setting up the main display window
 root = tk.Tk()
 root.title("INI File Editor")
 root.geometry("400x300")
+
+select_button = tk.Button(root, text="Select INI File", command=select_file)
+select_button.pack(pady=20)
+
 root.mainloop()
